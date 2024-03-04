7d9e3fc5d225f5c1c9673a40363fab376f963080
Riz0tto <104946042+Riz0tto@users.noreply.github.com>
2024-03-05 12:30:32 +1300

    Added circle grow/scroll effect

--- draw_one_frame.js
+++ draw_one_frame.js
@@ -1,20 +1,38 @@
-var fillColor = 0;
+var backgroundColor = 0;
+var shapeColor = 0;
+var gridSize = 10;
 
 function draw_one_frame(cur_frac) {
+	// set background colour to last shape colour on first frame	
+	if(cur_frac == 0) color_change();
+
+	// draw background using a rect
 	strokeWeight(0);
-	if(cur_frac == 0) color_flip();
-	if(fillColor == 0) fill(255);
-	else fill(0);
+	fill(backgroundColor);
 	rect(0, 0, width, height);
 
-	rectWidth = map(cur_frac, 0, 1, 0, width);
-	rectHeight = map(cur_frac, 0, 1, 0, height);
-	fill(fillColor);
-	rect(width/2-rectWidth/2, height/2-rectHeight/2, rectWidth, rectHeight);
+	// draw circles
+	ellipseMode(CENTER);
+	fill(shapeColor);
+
+	for(let i = 0; i < gridSize; i++) {		
+		for(let j = 0; j < gridSize*2; j++){
+			ellipseSize = map(cur_frac, 0, 1, 0, 2* width/gridSize);
+			scrollAmount = map(cur_frac, 0, 1, 0, 2 * width/gridSize);
+
+			if(i % 2 == 1) {
+				ellipse(i*(width/gridSize) + (width/gridSize/2), j*(width/gridSize) - scrollAmount, ellipseSize);
+			} else {
+				ellipse(i*(width/gridSize) + (width/gridSize/2), j*(width/gridSize) + scrollAmount - width, ellipseSize);
+			}
+		}
+
+	}
+
 }
 
-function color_flip() {
-	if(fillColor == 0) fillColor = 255;
-	else fillColor = 0;
+function color_change() {
+	backgroundColor = shapeColor;
+	shapeColor = color(random(255), random(255), random(255));
 }
 
