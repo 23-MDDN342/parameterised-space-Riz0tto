5404e3f07a62ba73eea1f16446b7ea2e356af5db
Riz0tto <104946042+Riz0tto@users.noreply.github.com>
2024-03-17 00:19:55 +1300

    Removed old code, now just the pool
    Also added a scroll effect, tidied up the way the lines are drawn, and added sparkle (uses random)

--- draw_one_frame.js
+++ draw_one_frame.js
@@ -1,86 +1,80 @@
 var backgroundColor = 300;
 var shapeColor = 0;
-var gridSize = 20;
-var numCurves = 20;
+var numPoolLines = 20;
 
 function draw_one_frame(cur_frac) {
-	colorMode(HSB, 300);
 
-	// set background colour to last shape colour on first frame	
-	//if(cur_frac == 0) color_change();
+	colorMode(HSB, 300);
 
 	// draw background using a rect
 	strokeWeight(0);
 	fill(160, 150, 300);
 	rect(0, 0, width, height);
 
-	// draw circles
-	//drawCircles(cur_frac);
+	let yScroll = map(cur_frac, 0, 1, 0, height/numPoolLines);
 
-	// curve
-	drawCurves(cur_frac);
-	push();
-	angleMode(DEGREES);
-	translate(height*(width/height), 0);
-	rotate(90);
-	scale(width/height);
-	drawCurves(cur_frac);
+	push();	
+	translate(0, yScroll);
+	drawPoolLines(cur_frac);
+	waterGlisten(cur_frac);		
 	pop();
 	
-}
-
-function drawCircles(cur_frac) {
-	ellipseMode(CENTER);
-	fill(shapeColor);
-
-	for(let i = 0; i < gridSize; i++) {		
-		for(let j = 0; j < gridSize*2; j++){
-			ellipseSize = map(cur_frac, 0, 1, 0, 2* width/gridSize);
-			scrollAmount = map(cur_frac, 0, 1, 0, 2 * width/gridSize);
-
-			if(i % 2 == 1) {
-				ellipse(i*(width/gridSize) + (width/gridSize/2), j*(width/gridSize) - scrollAmount, ellipseSize);
-			} else {
-				ellipse(i*(width/gridSize) + (width/gridSize/2), j*(width/gridSize) + scrollAmount - width, ellipseSize);
-			}
-		}
-	}	
-}
-
-function color_change(cur_frac) {
-	backgroundColor = shapeColor;
-	shapeColor = color(random(150, 200), 150, 300);
-}
-
-function drawCurves(cur_frac) {
-	let curveHeight;
 	
-	for(let j = 0; j < numCurves; j++) {
-		if(cur_frac <= 0.5){
-		curveHeight = map(cur_frac, 0, 0.5, 0, height);
-		} else {
-			 
-			curveHeight = map(cur_frac, 0.5,1, height, 0);
-		}
-			console.log(curveHeight)
-		push();
+}
 
-		//translate(0, curveHeight);
+function drawPoolLines(cur_frac) {	
+	let scrollOffset = 0 - (height/numPoolLines);
+	for (let j = 0; j < numPoolLines; j++) {		
+
 		stroke(0, 0, 300, 150);
 		strokeWeight(width/200);
 		noFill();
 
 		beginShape();
-		for(let i = -1; i <= gridSize+1; i++) {	
-			curveVertex(i*(width/gridSize), getNoiseValue(i*(width/gridSize), curveHeight + j*(height/numCurves), 1, "curveNoise", -25, 25, 400) + j*(height/numCurves));
+		for (let i = -1; i <= numPoolLines+1; i++) {	
+			let curveHeight = getAnimatedNoiseValue(cur_frac, i*(width/numPoolLines), j*(height/numPoolLines), width);
+			curveVertex(i*(width/numPoolLines), curveHeight + j*((height + Math.abs(scrollOffset))/numPoolLines) + (0.5*height/numPoolLines) + scrollOffset);
 		}
-
 		endShape();
 
-		//console.log(getNoiseValue(10*(width/gridSize), curveHeight + 10*(height/numCurves), 0.8, "curveNoise", -25, 25, 200) + 10*(height/numCurves))
-		pop();
+		beginShape();
+		for (let i = -1; i <= numPoolLines+1; i++) {	
+			let curveHeight = getAnimatedNoiseValue(cur_frac, i*(width/numPoolLines), j*(height/numPoolLines), height);
+			curveVertex(curveHeight + j*(width/numPoolLines) + (0.5*width/numPoolLines), i*((height + Math.abs(scrollOffset))/numPoolLines) + scrollOffset);
+		}
+		endShape();
 	}
 }
+
+function getAnimatedNoiseValue(cur_frac, posX, posY, resolutionValue) {
+
+	let animScroll;
+	if (cur_frac <= 0.5){
+		animScroll = map(cur_frac, 0, 0.5, 0, resolutionValue);
+		} 			
+	else animScroll = map(cur_frac, 0.5, 1, resolutionValue, 0);
+
+	if (resolutionValue == width) return getNoiseValue(posX + animScroll, posY, 1, "animatedNoise", -20, 20, 1000);
+	if (resolutionValue == height) return getNoiseValue(posX, posY + animScroll, 1, "animatedNoise", -20, 20, 1000);
+
+}
+
+function waterGlisten() {
+	push();
+
+	for (let i = 0; i < 20; i++) {
+		for (let j = 0; j < 20; j++) {
+			if (random(1) > 0.95) {
+				strokeWeight(width/100 * random(1));
+				stroke(300, 0, 300, 300 * random(1));
+				point(i*width/20, j*height/20);
+			}
+		}
+	}
+
+	pop();
+}
+
 
 /* 
    getNoiseValue arguments:
